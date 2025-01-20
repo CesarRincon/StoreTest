ac7c48588ccc9de1effe3f16ce51119b6e1b617d
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2025-01-19 19:54:35 -0500

    ST-ADD: AddToCart button added and completed

---
+++ src/components/ButtonAddToCart/ButtonAddToCartStyles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const buttonAddStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "#000",
+        width: "90%",
+        height: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 8
+    },
+    text: {
+        color: "#fff"
+    }
+});
