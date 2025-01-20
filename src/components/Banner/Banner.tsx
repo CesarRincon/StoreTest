94f4e88755f08d2a24158f5b25ab2b0bc6d3833c
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2025-01-20 11:30:45 -0500

    ST-ADD: banner test

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,8 +4,12 @@ import { bannerStyles } from './BannerStyles'
 
 const Banner = () => {
     return (
-        <View style={bannerStyles.container}>
+        <View
+            testID="banner-container"
+            style={bannerStyles.container}
+        >
             <Image
+                testID="banner-image"
                 source={{ uri: "https://img.freepik.com/vector-gratis/plantilla-banner-horizontal-ventas-dia-compras-12-12_23-2149851470.jpg?t=st=1736970568~exp=1736974168~hmac=951a099a288bf6a67167e8b3d4c1c5b21296161e808780936bc869b49a19306f&w=2000" }}
                 width={500}
                 height={120}
