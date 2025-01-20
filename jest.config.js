bb8f5863f7b994caea5579352a5f07129828d55c
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2025-01-20 11:29:31 -0500

    ST-ADD: Config jest and install

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   preset: 'react-native',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.{js,ts}',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/components/utils/utils.ts',
+    '<rootDir>/src/services/services.ts',
+    '<rootDir>/src/hooks/useCartPetitions.tsx',
+    '<rootDir>/src/hooks/useGetColombianInfo.tsx',
+    '<rootDir>/src/hooks/useLinkTo.tsx',
+  ],
+  coverageDirectory: './coverage',
+  testPathIgnorePatterns: ['/android/', '/ios/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-redux|@react-native|react-native|redux)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {},
 };
